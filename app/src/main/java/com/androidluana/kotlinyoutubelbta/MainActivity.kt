79dd2b9c6bf9c926749f014d1633c22391b480d8
Luana Franca <21898@student.dorset-college.ie>
2021-03-20 22:56:12 +0000

    Third Commit - Image Loading Picasso

--- app/src/main/java/com/androidluana/kotlinyoutubelbta/MainActivity.kt
+++ app/src/main/java/com/androidluana/kotlinyoutubelbta/MainActivity.kt
@@ -57,13 +57,5 @@ class MainActivity : AppCompatActivity() {
 
 }
 
-class HomeFeed(val videos: List<Video>)
-
-class Video(
-    val id: Int, val name: String, val link: String, val imageUrl: String,
-    val numberOfViews: Int, val channel: Channel
-)
-
-class Channel(val name: String, val profileImageUrl: String)
 
 
--- app/src/main/java/com/androidluana/kotlinyoutubelbta/MainAdapter.kt
+++ app/src/main/java/com/androidluana/kotlinyoutubelbta/MainAdapter.kt
@@ -4,10 +4,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.video_row.view.*
 
 
-class MainAdapter(val homeFeed: HomeFeed): RecyclerView.Adapter<CustomViewHolder>() {
+class MainAdapter(val homeFeed: HomeFeed) : RecyclerView.Adapter<CustomViewHolder>() {
 
     val videoTitles = listOf<String>("First title", "Second", "3rd", "MOOOOORE TITLE")
 
@@ -19,7 +20,7 @@ class MainAdapter(val homeFeed: HomeFeed): RecyclerView.Adapter<CustomViewHolder
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
 
         val layoutInflater = LayoutInflater.from(parent.context)
-        val cellForRow = layoutInflater.inflate(R.layout.video_row,parent,false)
+        val cellForRow = layoutInflater.inflate(R.layout.video_row, parent, false)
         return CustomViewHolder(cellForRow)
     }
 
@@ -28,10 +29,19 @@ class MainAdapter(val homeFeed: HomeFeed): RecyclerView.Adapter<CustomViewHolder
 //        val videoTitle = videoTitles.get(position)
         val video = homeFeed.videos.get(position)
         holder.view.textView_video_title.text = video.name
+
+        holder.view.textView_channel_name.text = video.channel.name + "  •  " + "20k Views\n4 days ago"
+
+        val thumbnailImageView = holder.view.imageView_video_thumbnail
+        Picasso.with(holder.view.context).load(video.imageUrl).into(thumbnailImageView)
+
+        val channelProfileImageView = holder.view.imageView_channel_profile
+        Picasso.with(holder.view.context).load(video.channel.profileImageUrl).into(channelProfileImageView)
+
     }
 }
 
-class CustomViewHolder(val view: View): RecyclerView.ViewHolder(view) {
+class CustomViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
 
 
 }
